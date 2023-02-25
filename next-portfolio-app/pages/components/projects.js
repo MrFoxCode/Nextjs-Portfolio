324b1650242a35451a55b3a656610ee2ac5631e6
Mr. Fox <fox.contas@hotmail.com>
2023-02-25 01:22:16 -0300

    some adjusts to projects section

--- next-portfolio-app/pages/components/projects.js
+++ next-portfolio-app/pages/components/projects.js
@@ -3,8 +3,14 @@ import React from "react";
 function Projects(){
     return(
         <div className="projects-container">
-
+            
+            <div className="project-tittle-container">
+                <h2>My Projects</h2>
+            </div>
+            
             <div className="projects-content">
+               
+
                 <div className="projects-left-container">
                 </div>
                 
